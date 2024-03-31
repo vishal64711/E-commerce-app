fadaddc81057f9fb1f54ab52d891558a5485bd75
Vishal <vishal64711@gmail.com>
2024-03-31 19:02:23 +0530

    Userschema and authentication

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from "express";
 import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import { connect } from "mongoose";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 //configure env
 dotenv.config();
@@ -18,6 +18,9 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
 
 //rest api
 app.get("/", (req, res) => {
