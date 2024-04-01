80fa2547190cac1c412fcdb372ba672abfd87ba6
Vishal <vishal64711@gmail.com>
2024-04-01 14:09:07 +0530

    RegisterController authController

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -10,6 +10,6 @@ export const hashPassword = async (password) => {
   }
 };
 
-export const comparePassword = async (password, hashPassword) => {
+export const comparePassword = async (password, hashedPassword) => {
   return bcrypt.compare(password, hashedPassword);
 };
